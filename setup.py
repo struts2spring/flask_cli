c676140820d9cf58acbb0fcde30027fb64edea5c
vijay <struts2spring@gmail.com>
2017-11-26 11:41:44 +0530

    save

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name='Flask cli',
+      version='0.1',
+      description='Flask cli (commond line interface) for Flask application based project',
+      author='Vijay ',
+      author_email='struts2spring@gmail.com',
+      url='',
+      packages=['src',],
+     )
